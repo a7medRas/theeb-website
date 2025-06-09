5702974d54ec02437948d43ea2f3b9903cbfa9b8
a7medRas <64759784+a7medRas@users.noreply.github.com>
2025-06-10 00:04:00 +0300

    Add files via upload

---
+++ events.js
@@ -0,0 +1,9 @@
+
+export default function Events() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-semibold">Events Page</h1>
+      <p>Upcoming and past events will be listed here.</p>
+    </div>
+  );
+}
---
+++ news.js
@@ -0,0 +1,9 @@
+
+export default function News() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-semibold">News Page</h1>
+      <p>All updates and articles will be shown here.</p>
+    </div>
+  );
+}
