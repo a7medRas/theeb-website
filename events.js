416a4893b74af251d79572c29c41eaa4ee3a2e77
a7medRas <64759784+a7medRas@users.noreply.github.com>
2025-06-10 00:15:13 +0300

    Delete events.js

--- events.js
+++
@@ -1,9 +0,0 @@
-
-export default function Events() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-semibold">Events Page</h1>
-      <p>Upcoming and past events will be listed here.</p>
-    </div>
-  );
-}
